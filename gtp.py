cd1c10016384d3fc0aaf815d41f5509ef1168c72
livexia <istillalive@live.cn>
2023-12-30 20:16:50 +0800

    gtp as server and engine as client

--- gtp.py
+++ gtp.py
@@ -5,7 +5,7 @@ import time
 from typing import Optional
 
 
-def spawn_gnugo(host: str, port: int) -> subprocess.Popen:
+def spawn_gnugo_server(host: str, port: int) -> subprocess.Popen:
     proc = subprocess.Popen(
         "gnugo --mode gtp --gtp-listen {}:{}".format(host, port),
         shell=True,
@@ -13,12 +13,25 @@ def spawn_gnugo(host: str, port: int) -> subprocess.Popen:
         stdout=subprocess.PIPE,
     )
 
-    print("Spawn gnugo at {}:{} pid: {}".format(host, port, proc.pid))
+    print("Spawn gnugo(pid:{}) lsiten at {}:{}".format(proc.pid, host, port))
     time.sleep(2)
     return proc
 
 
-def connect_to_gtp(host: str, port: int) -> socket.SocketType:
+def spawn_gnugo_client(host: str, port: int) -> subprocess.Popen:
+    proc = subprocess.Popen(
+        "gnugo --mode gtp --gtp-connect {}:{}".format(host, port),
+        shell=True,
+        stdin=subprocess.PIPE,
+        stdout=subprocess.PIPE,
+    )
+
+    print("Spawn gnugo(pid:{}) connect to {}:{}".format(proc.pid, host, port))
+    time.sleep(2)
+    return proc
+
+
+def gtp_client(host: str, port: int) -> socket.SocketType:
     # Create a TCP/IP socket
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
@@ -29,8 +42,18 @@ def connect_to_gtp(host: str, port: int) -> socket.SocketType:
     return sock
 
 
-def send_command(gtp, command: bytes) -> Optional[bytearray]:
-    gtp.send(command)
+def gtp_server(host: str, port: int):
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+    address = (host, port)
+    print("listening to {} port {}".format(*address))
+    sock.bind(address)
+    sock.listen(1)
+    return sock
+
+
+def send_command(connection, command: bytes) -> Optional[bytearray]:
+    connection.send(command)
 
     data = bytearray()
 
@@ -38,10 +61,59 @@ def send_command(gtp, command: bytes) -> Optional[bytearray]:
         if command == b"quit\n":
             print("quit")
             raise ConnectionResetError
-        data.extend(gtp.recv(4096))
+        data.extend(connection.recv(4096))
         if len(data) == 0 or data[-2:] == b"\n\n":
             break
     return data
+
+
+def client(host: str, port: int):
+    spawn_gnugo_server(host, port)
+    gtp = gtp_client(host, port)
+    try:
+        while True:
+            # Send data
+            message = bytes("{}\n".format(input("> ")), "utf-8")
+            print("sending {!r}".format(message))
+            data = send_command(gtp, message)
+
+            if data is not None:
+                print("received\n {}".format(str(data, "utf-8")))
+            else:
+                raise
+
+    except Exception as e:
+        print("closing socket because: {!r}".format(e))
+        gtp.close()
+    except KeyboardInterrupt as e:
+        print(e)
+        gtp.close()
+
+
+def server(host: str, port: int):
+    gtp = gtp_server(host, port)
+    spawn_gnugo_client(host, port)
+    try:
+        # Wait for a connection
+        client, addr = gtp.accept()
+        print("connection from", addr)
+        while True:
+            # Send data
+            message = bytes("{}\n".format(input("> ")), "utf-8")
+            print("sending {!r}".format(message))
+            data = send_command(client, message)
+
+            if data is not None:
+                print("received\n {}".format(str(data, "utf-8")))
+            else:
+                raise
+
+    except Exception as e:
+        print("closing socket because: {!r}".format(e))
+        gtp.close()
+    except KeyboardInterrupt as e:
+        print(e)
+        gtp.close()
 
 
 class PortNumber(int):
@@ -67,35 +139,23 @@ def main():
         type=PortNumber,
         required=True,
         help="Connect port",
-        metavar="[1..65535]",
+        metavar="{1..65535}",
+    )
+    parser.add_argument(
+        "-m",
+        dest="mode",
+        choices=["client", "server"],
+        default="server",
     )
     args = parser.parse_args()
 
     host = args.host
     port = args.port
 
-    spawn_gnugo(host, port)
-
-    gtp = connect_to_gtp(host, port)
-
-    try:
-        while True:
-            # Send data
-            message = bytes("{}\n".format(input("> ")), "utf-8")
-            print("sending {!r}".format(message))
-            data = send_command(gtp, message)
-
-            if data is not None:
-                print("received\n {}".format(str(data, "utf-8")))
-            else:
-                raise
-
-    except Exception as e:
-        print("closing socket because: {!r}".format(e))
-        gtp.close()
-    except KeyboardInterrupt as e:
-        print(e)
-        gtp.close()
+    if args.mode == "client":
+        client(host, port)
+    elif args.mode == "server":
+        server(host, port)
 
 
 if __name__ == "__main__":
